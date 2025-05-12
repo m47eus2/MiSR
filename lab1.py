710e36bdb6cb853e87c578cbd8e2a923307b306f
m47eus2 <mateusz.ciesielski2003@gmail.com>
2025-05-12 16:41:36 +0200

    Environment configuration

---
+++ lab1.py
@@ -0,0 +1,25 @@
+# wczytanie potrzebnych podczas zajęć bibliotek:
+import roboticstoolbox as rtb
+import numpy as np
+from spatialmath import *
+from spatialmath.base import *
+from spatialmath.base.symbolic import *
+from matplotlib import pyplot as plt
+# ...
+
+# definicje funkcji:
+def przyklad_1():
+    R1 = SO3.Rx(0.3)
+    print(R1)
+
+def zadanie_1():
+    pass # zastąp tę linię swoim kodem
+
+def zadanie_2():
+    pass # zastąp tę linię swoim kodem
+
+# ...
+
+# wykonywanie wybranej funkcji
+if __name__ == '__main__':
+    przyklad_1()
